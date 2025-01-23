bfece598df733a1e551a3b3a991dbf6ce4f76e33
unknown <your@email.example>
2025-01-23 13:01:25 +0200

    first commit

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+<x-layout>
+
+    <h1>Products</h1>
+
+    <a href="{{ route('products.create') }}">New product</a>
+
+    @foreach ($products as $product)
+        <h2><a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a></h2>
+        <p>{{ ($product->description) }}</p>
+        <p> {{ $product->size }}</p>
+    @endforeach
+
+
+</x-layout>
